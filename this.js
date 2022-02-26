3f96da6a694df66a94aedf735a7d0bb8cf250ca4
ahsan <makabir98@yahoo.com>
2022-02-26 22:10:05 +0600

    this is tough

--- this.js
+++ this.js
@@ -1,9 +1,7 @@
-
-
-
+//console.log(this)
 const kibria = {
     id : 101,
-    money: 5000,
+    money: 50000,
     name: 'RJ Kibria',
     major : 'mathematics',
     treatDe: function(expense, bokshish){
@@ -18,16 +16,3 @@ const heroBalan = {
     money: 6000,
     name : 'Hero Balam'
 }
-
-const normalGolam = {
-    id: 103,
-    money: 8000,
-    name : 'Normal Golam'
-}
-
-
-//call
-kibria.treatDe.call(heroBalan, 200, 100);
-
-//apply
-kibria.treatDe.apply(normalGolam,[500, ])
